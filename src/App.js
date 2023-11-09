c443f57583ed272a3513b152528a4fd097ac4039
Mohamed charfeddine <medcharfeddine@outlook.com>
2023-11-09 13:05:34 +0100

    modification

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
 import React from "react";
+import "./App.css"
 
 const App = () => {
+  const style = {
+    border : "none",
+    overFlow: "hidden"
+  }
   return (
-    <div>
-      <img
-        src="https://www.get.com.tn/frontend/img/logo-Get.png"
-        alt="logo"
-      />
-      <h2>Follow Us</h2>
-      <div id="like">
-        <div
-          class="fb-like"
-          data-href="https://www.facebook.com/GET.Fashion"
-          data-width="300"
-          data-layout=""
-          data-action=""
-          data-size="large"
-          data-share="false"
-          style={{ color: "aliceblue" }}
-        ></div>
-      </div>
-      <div>
-        <div id="fb-root"></div>
-        <script
-          async
-          defer
-          crossorigin="anonymous"
-          src="https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v18.0&appId=737480571558324"
-          nonce="hSGvKLLb"
-        ></script>
+    <div className="App">
+      <div class="container">
+        <img
+          src="https://www.get.com.tn/frontend/img/logo-Get.png"
+          alt="logo"
+        />
+        <h2>Follow Us</h2>
+        <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FGET.Fashion&width=300&layout&action&size&share=false&height=35&appId=737480571558324" width="300" height="35" style={style} scrolling="no" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
       </div>
     </div>
   );
