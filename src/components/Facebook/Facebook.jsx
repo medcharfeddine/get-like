d454291e348e45bed14d834e40ef12879105f37d
Mohamed charfeddine <medcharfeddine@outlook.com>
2023-11-25 11:08:06 +0100

    change facebook link

--- src/components/Facebook/Facebook.jsx
+++ src/components/Facebook/Facebook.jsx
@@ -2,17 +2,28 @@ import React from "react";
 import FacebooK from "../../assets/facebook.png";
 
 const Facebook = () => {
+  const isAndroid = /Android/.test(navigator.userAgent);
+  const isIOS = /iPhone|iPad|iPod/.test(navigator.userAgent);
   return (
     <div className="socials">
-      <a
+      {isAndroid && <a
         href="fb://page/924897634241785"
         target="_blank"
         rel="noreferrer"
         style={{ display: "flex", justifyContent: "center" }}
       >
         <img className="image" src={FacebooK} alt="facebook" />
-      <h1>Facebook</h1>
-      </a>
+        <h1>Facebook</h1>
+      </a>}
+      {isIOS && <a
+        href="fb://profile/924897634241785"
+        target="_blank"
+        rel="noreferrer"
+        style={{ display: "flex", justifyContent: "center" }}
+      >
+        <img className="image" src={FacebooK} alt="facebook" />
+        <h1>Facebook</h1>
+      </a>}
     </div>
   );
 };
